48f6ee98039dce200b26db0d723dad5dc255ddd9
RThong <403216075@qq.com>
2022-08-16 14:20:58 +0800

    doc: update doc

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -8,19 +8,25 @@ import React from 'react'
 import LineChart from '../components/LineChart'
 import styles from './index.module.css'
 
+const Svg = require('@site/static/img/undraw_segmentation_re_gduq.svg').default
+
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+      <main className={styles.layout}>
+        <div className={styles.introHeader}>
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link className="button button--outline button--info button--lg" to="/docs/intro">
+              Demo
+            </Link>
+          </div>
         </div>
-      </div>
+
+        <Svg role="img" className={styles.svg} />
+      </main>
     </header>
   )
 }
@@ -33,7 +39,7 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
-      <main className={styles.main}>
+      <main className={clsx(styles.layout, styles.main)}>
         <h1>Installation</h1>
         <CodeBlock language="sh" showLineNumbers>
           pnpm add use-react-echarts -S
